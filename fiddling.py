48b1294aec08f1a264c24c64f2a623887626888b
Alex Rattray <rattray.alex@gmail.com>
2013-01-19 13:48:04 -0500

    fiddling.py now has functions! yay. including diffinator

--- fiddling.py
+++ fiddling.py
@@ -2,21 +2,71 @@
 import SimpleCV
 import time, sys
 
-cam = SimpleCV.Camera(1)
+cam = SimpleCV.Camera(0)
 disp = SimpleCV.Display()
-i = 0
-dirname = sys.argv[1]
+prev = None
 
-while disp.isNotDone():
-  if disp.mouseLeft:
-    break
-  # img = cam.getImage()
-  img = SimpleCV.Image('%s/%s.jpg' % (dirname, i))
+def load_play(func):
+  i = 0
+  dirname = sys.argv[1]
+
+  while disp.isNotDone():
+    if disp.mouseLeft:
+      break
+    img = SimpleCV.Image('%s/%s.jpg' % (dirname, i))
+    if func:
+      img = func(img)
+    img.save(disp)
+    i += 1
+
+def record():
+  i = 0
+  dirname = sys.argv[1]
+  try:
+    os.mkdir(dirname)
+  except:
+    print 'could not make dir'
+    exit()
+
+  while disp.isNotDone():
+    if disp.mouseLeft:
+      break
+    img.save(disp)
+    img.save('%s/%s.jpg' % (dirname, i))
+    i += 1
+
+def play(func=None):
+  while disp.isNotDone():
+    if disp.mouseLeft:
+      break
+    img = cam.getImage()
+    if func: 
+      img = func(img)
+    img.save(disp)
+
+def drawlines(img):
   blobs = img.findLines()
   blobs.draw()
-  img.save(disp)
-  i += 1
+  return img
 
-disp.quit()
+def diffinator(img):
+  global prev
+  if not prev:
+    prev = img
+    return img
+  else:
+    diff = img - prev
+    prev = img
+    matrix = diff.getNumpy()
+    mean = matrix.mean()
+    print mean
+    return diff
 
-time.sleep(.1)
+def main():
+  # loadimgs()
+  load_play(diffinator)
+  disp.quit()
+  time.sleep(.1)
+
+if __name__ == '__main__':
+  main()
