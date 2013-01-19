041dc509991d27b080eda8fa26108cc47e26a169
Alex Rattray <rattray.alex@gmail.com>
2013-01-19 11:43:54 -0500

    recorded dir-image 'videos'

--- record.py
+++ record.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python
 import SimpleCV
-import time, sys
+import time, sys, os
 
 cam = SimpleCV.Camera(1)
 disp = SimpleCV.Display()
 i = 0
+
 dirname = sys.argv[1]
+try:
+  os.mkdir(dirname)
+except:
+  exit()
 
 while disp.isNotDone():
   if disp.mouseLeft:
     break
-  # img = cam.getImage()
-  img = SimpleCV.Image('%s/%s.jpg' % (dirname, i))
-  blobs = img.findLines()
-  blobs.draw()
+  img = cam.getImage()
+  # blobs = img.findLines()
+  # blobs.draw()
   img.save(disp)
+  img.save('%s/%s.jpg' % (dirname, i))
   i += 1
 
 disp.quit()
