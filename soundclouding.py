7c049b97f490b40ec586a9874e2349d76f603d96
Alex Rattray <rattray.alex@gmail.com>
2013-01-19 23:18:55 -0500

    built playlist

--- soundclouding.py
+++ soundclouding.py
@@ -5,6 +5,19 @@ import time
 class SoundStreamer:
   def __init__(self):
     self.client = soundcloud.Client(client_id='045b8c0a4960cce57af82f215695fb3b')
+    self.playlist = [
+      ('party-pants', 36247624),
+      ('party-pants-1', 36894638),
+      ('party-pants-2', 37662755),
+      ('party-pants-3', 38415258),
+      ('party-pants-6', 40651214),
+      ('party-pants-8', 42484028),
+      ('party-pants-9', 46637250),
+      ('party-pants-10', 46639797),
+      ('party-pants-12', 61522767),
+      ('party-pants-13', 71527983),
+      ('party-pants-14', 71533432),
+    ]
     #creates a playbin (plays media form an uri) 
     self.player = gst.element_factory_make("playbin", "player")
 
@@ -12,11 +25,19 @@ class SoundStreamer:
     artist_id = self.client.get('/users', q=artist)[0].id
     tracks = self.client.get('/tracks', user_id=artist_id)
     tracks.sort(key = lambda t: -t.playback_count)
-    for t in tracks: print t.download_count, t.playback_count, t.permalink
+    for t in tracks: print t.download_count, t.playback_count, t.permalink, t.stream_url
     return tracks
 
+  # Credit the uploader as the creator of the sound
+  # Credit SoundCloud as the source by including one of the logos found here
+  # Link to the SoundCloud URL containing the work
+  # If the sound is private link to the profile of the creator
+
   def get_track_url(self, track):
-    # track = self.client.get('/tracks/%s' % track_id)
+    try:
+      track = self.client.get('/tracks/%s' % track)
+    except:
+      pass
     stream_url = self.client.get(track.stream_url, allow_redirects=False)
     print stream_url.location
     return stream_url.location
